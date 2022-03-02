95c17bad0d0342edff3c2deb310762eb3e22817c
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2022-03-02 00:59:16 -0400

    feat: Transfer to isolated interface file modal config

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, TemplateRef } from "@angular/core";
+import { ModalConfig } from "../interfaces/modal-config";
 
 @Injectable()
 export class ModalService {
@@ -8,10 +9,7 @@ export class ModalService {
   }
 }
 
-export interface ModalConfig {
-    templateRef: TemplateRef<any>;
-    title: string;
-}
+
 
 export class ModalRef {
 
